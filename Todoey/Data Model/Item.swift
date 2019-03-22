53462eceec85b32b57d1ecf131bfef19310e2cd1
Kevin Mcphail <kevinmcphail@kevins-macbook-air.local>
2019-03-22 10:42:29 -0400

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kevin Mcphail on 3/22/19.
+//  Copyright © 2019 Adisseo. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
